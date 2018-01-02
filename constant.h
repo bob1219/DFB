6d9c9a03dece697c6346264f8be9d8609a20d371
吉田大輝 <bob181219@yahoo.co.jp>
2018-01-02 17:39:03 +0900

    Addition declarations of constants "PATH_BREAK_CHARACTER" and "FILE_SIZE_MAX"

---
+++ constant.h
@@ -0,0 +1,15 @@
+#ifndef _CONSTANT_H_
+#define _CONSTANT_H_
+
+// Standard Library
+#include <cstddef>
+
+namespace dfb
+{
+	using namespace std;
+
+	static const char	PATH_BREAK_CHARACTER	= '\\';
+	static const size_t	FILE_SIZE_MAX		= 102400;
+}
+
+#endif
