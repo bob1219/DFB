789e6074ea3b0883972e1150a718bb07bbb59511
吉田大輝 <bob181219@yahoo.co.jp>
2018-01-02 17:07:25 +0900

    Describe command::run function

--- commands.cpp
+++ commands.cpp
@@ -57,3 +57,29 @@ bool command::clear()
 
 	return true;
 }
+
+bool command::run()
+{
+	// open file
+	char from_filename[FILENAME_MAX], to_filename[FILENAME_MAX];
+	sprintf(from_filename, ".%csetting%cfrom", PATH_BREAK_CHARACTER, PATH_BREAK_CHARACTER);
+	sprintf(to_filename, ".%csetting%cto", PATH_BREAK_CHARACTER, PATH_BREAK_CHARACTER);
+	ifstream from_fs(from_filename), to_fs(to_filename);
+	if(from_fs.fail() || to_fs.fail())
+		return false;
+
+	// backup
+	string from_line, to_line;
+	char buf[FILE_SIZE_MAX];
+	while(getline(from_fs, from_line) && getline(to_fs, to_line))
+	{
+		ifstream backup_from_fs(from_line, ios_base::in | ios_base::binary), backup_to_fs(to_line, ios_base::in | ios_base::binary);
+		if(backup_from_fs.fail() || backup_to_fs_fail())
+			return false;
+
+		backup_from_fs.read(buf, (sizeof(buf) / sizeof(char)));
+		backup_to_fs.write(buf, (sizeof(buf) / sizeof(char)));
+	}
+
+	return true;
+}
